d4d9a2e7ca28ea0f23351f4e49e594bb1ee95fb2
Thanh Tran <thanhtngcd17200@fpt.edu.vn>
2020-03-09 12:01:51 +0700

    merge file

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,224 +1,100 @@
 <template>
-    <div class="login-container">
-        <el-form
-            ref="loginForm"
-            :model="loginForm"
-            :rules="loginRules"
-            class="login-form"
-            autocomplete="on"
-            label-position="left"
-        >
-            <el-image :src="url" />
+    <div class="login container">
+        <form 
+            @submit.prevent="login" 
+            class="card-panel">
 
-            <el-form-item prop="username">
-                <el-input
-                ref="username"
-                v-model="loginForm.username"
-                placeholder="Email"
-                name="username"
-                type="text"
-                tabindex="1"
-                autocomplete="on"
-                />
-            </el-form-item>
-
-            <el-tooltip
-                v-model="capsTooltip"
-                content="Caps lock is On"
-                placement="right"
-                manual
-            >
-                <el-form-item prop="password">
-                <el-input
-                    :key="passwordType"
-                    ref="password"
-                    v-model="loginForm.password"
-                    :type="passwordType"
-                    placeholder="Password"
-                    name="password"
-                    tabindex="2"
-                    autocomplete="on"
-                    @keyup.native="checkCapslock"
-                    @blur="capsTooltip = false"
-                    @keyup.enter.native="handleLogin"
-                />
-                </el-form-item>
-            </el-tooltip>
-
-            <div v-if="showLog">
-                <ErrorDialog :textMsg="messages"/>
-            </div>
-
-
-            <el-button
-                class="btn-submit"
-                :loading="loading"
-                @click.native.prevent="handleLogin"
-            >
+            <h2 class="center pink-text text-lighten-1">
                 Login
-            </el-button>
-        </el-form>
+            </h2>
+            <div class="field">
+                <label for="email">
+                    Email
+                </label>
+                <input type="text" name="email" 
+                    v-model="email">
+            </div>
+            <div class="field">
+                <label for="password">
+                    Password
+                </label>
+                <input type="password" name="password" 
+                    v-model="password">
+            </div>
+            <p class="red-text center" 
+                v-if="feedback">
+                {{ feedback }}
+            </p>
+            <div class="field center">
+                <button class="btn pink lighten-1">
+                    login
+                </button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
-import ErrorDialog from '@/components/Dialog/ErrorDialog'
-import { validEmail } from '@/utils/validate'
 import firebase from 'firebase'
-
 export default {
     name: 'login',
-    components: { ErrorDialog },
     data(){
-        const validateUsername = (rule, value, callback) => {
-            if (value.length < 5) {
-                callback(new Error('Please enter the correct username or email'))
-            } else if (!validEmail(value)) {
-                callback(new Error('Email is incorrect'))
-            } else {
-                callback()
-            }
-        }
-        const validatePassword = (rule, value, callback) => {
-            if (value.length < 6) {
-                callback(new Error('The password can not be less than 6 digits'))
-            } else {
-                callback()
-            }
-        }
         return {
-            loginForm: {
-                username: '',
-                password: ''
-            },
-            loginRules: {
-                username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-                password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-            },
-            showFormResetPassword: false,
-            passwordType: 'password',
-            capsTooltip: false,
-            loading: false,
-            messages: null,
-            showLog: false,
-            url: require('@/assets/logo.png')
-        }
-    },
-    mounted() {
-        //focus 
-        if (this.loginForm.username === '') {
-            this.$refs.username.focus()
-        } else if (this.loginForm.password === '') {
-            this.$refs.password.focus()
+            email: null,
+            password: null,
+            feedback: null
         }
     },
     methods:{
-        checkCapslock(e) {
-            const { key } = e
-            this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
-        },
-        handleLogin() {
-            this.$refs.loginForm.validate(valid => {
-                if (valid) {
-                    this.loading = true
-                    this.showLog = false
-
-                    firebase.auth().signInWithEmailAndPassword(this.loginForm.username, this.loginForm.password).then(cred =>
-                    {
-                        this.loading = false
-                        this.$router.push({ name: 'HomeUser'}) //after login, direct to home user page
-                    }).catch(err => {
-                        this.loading = false
-                        this.showLog = true
-                        this.messages = err
-                    })
+        login(){
+            //validation
+            if(this.email && this.password){
+                this.feedback = null
+                //sign in to the web using firebase method
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(cred =>
+                {
+                this.$router.push({ name: 'HomeUser'}) //after login, direct to home user page
+                }).catch(err => {
+                    alert(err)
+                })
+            }
+            else{
+                if(!this.email && this.password)
+                {
+                    this.feedback = "Please enter your Email"
                 }
-            })
-        },
+                else if(this.email && !this.password)
+                {
+                    this.feedback = "Please enter your Password"
+                }
+                else
+                {
+                    this.feedback = "Please enter your Email and password"
+                }
+            }
+
+        }
     },
     beforeCreate() {
-        document.body.className = "body-bg-image";
+    document.body.className = "body-bg-image";
     }
 
 }
 </script>
 
-<style lang="scss" scoped>
-.login-container {
-    min-height: 100%;
-    width: 100%;
-    overflow: hidden;
-    position: absolute;
-
-    .login-form {
-        position: relative;
-        width: 520px;
-        max-width: 100%;
-        padding: 0 35px 0;
-        margin: 0 auto;
-        overflow: hidden;
-        background: #ffffff;
-        margin-top: 50px;
-
-        .btn-submit {
-            width: 100%;
-            border: none;
-            border-radius: 7px;
-            padding: 12px;
-            margin-bottom: 50px;
-            background: #ec407a;
-            color: #ffffff;
-            margin-top: 20px;
-        }
+<style>
+    .login{
+        max-width: 500px;
+        margin-top: 60px;
     }
-}
-</style>
-
-<style lang="scss">
-/* reset element-ui css */
-.login-container {
-    .el-image {
-        width: 100%;
-        text-align: center;
-        margin-top: 50px;
-
-        img {
-            width: 100%;
-            height: 230px;
-        }
+    .login h2{
+        font-size: 2.4em;
     }
-
-    .el-input {
-        display: inline-block;
-        height: 47px;
-        width: 100%;
-        float: left;
-
-        input {
-            background: transparent;
-            border: 0px;
-            -webkit-appearance: none;
-            border-radius: 0px;
-            padding: 0;
-            margin-bottom: 0;
-            color: #000;
-            height: 47px;
-        }
+    .login .field{
+        margin-bottom: 16px;
     }
-
-    .el-form-item {
-        margin-top: 20px;
-
-        .el-input {
-            border-bottom: 1px solid #BDBDBD;
-            color: #454545;
-        }
+    label{
+        color: #000;
+        font-size: 16px;
     }
-
-    .el-form-item__content {
-        .el-form-item__error {
-            color: #ff0000;
-        }
-    }
-}
 </style>
