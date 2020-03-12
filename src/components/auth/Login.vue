c636c61a4fcd73709d7537db9f357ad79b02053f
cuongndn1506 <cuongndngcd17192@fpt.edu.vn>
2020-03-12 17:38:43 +0700

    Add all file

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="login-container">
+        <el-form
+            ref="loginForm"
+            :model="loginForm"
+            :rules="loginRules"
+            class="login-form"
+            autocomplete="on"
+            label-position="left"
+        >
+            <el-image :src="url" />
+
+            <el-form-item prop="username">
+                <el-input
+                ref="username"
+                v-model="loginForm.username"
+                placeholder="Email"
+                name="username"
+                type="text"
+                tabindex="1"
+                autocomplete="on"
+                />
+            </el-form-item>
+
+            <el-tooltip
+                v-model="capsTooltip"
+                content="Caps lock is On"
+                placement="right"
+                manual
+            >
+                <el-form-item prop="password">
+                <el-input
+                    :key="passwordType"
+                    ref="password"
+                    v-model="loginForm.password"
+                    :type="passwordType"
+                    placeholder="Password"
+                    name="password"
+                    tabindex="2"
+                    autocomplete="on"
+                    @keyup.native="checkCapslock"
+                    @blur="capsTooltip = false"
+                    @keyup.enter.native="handleLogin"
+                />
+                </el-form-item>
+            </el-tooltip>
+
+            <div v-if="showLog">
+                <ErrorDialog :textMsg="messages"/>
+            </div>
+
+
+            <el-button
+                class="btn-submit"
+                :loading="loading"
+                @click.native.prevent="handleLogin"
+            >
+                Login
+            </el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+import ErrorDialog from '@/components/Dialog/ErrorDialog'
+import { validEmail } from '@/utils/validate'
+import firebase from 'firebase'
+
+export default {
+    name: 'login',
+    components: { ErrorDialog },
+    data(){
+        const validateUsername = (rule, value, callback) => {
+            if (value.length < 5) {
+                callback(new Error('Please enter the correct username or email'))
+            } else if (!validEmail(value)) {
+                callback(new Error('Email is incorrect'))
+            } else {
+                callback()
+            }
+        }
+        const validatePassword = (rule, value, callback) => {
+            if (value.length < 6) {
+                callback(new Error('The password can not be less than 6 digits'))
+            } else {
+                callback()
+            }
+        }
+        return {
+            loginForm: {
+                username: '',
+                password: ''
+            },
+            loginRules: {
+                username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+                password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+            },
+            showFormResetPassword: false,
+            passwordType: 'password',
+            capsTooltip: false,
+            loading: false,
+            messages: null,
+            showLog: false,
+            url: require('@/assets/logo.png')
+        }
+    },
+    mounted() {
+        //focus
+        if (this.loginForm.username === '') {
+            this.$refs.username.focus()
+        } else if (this.loginForm.password === '') {
+            this.$refs.password.focus()
+        }
+    },
+    methods:{
+        checkCapslock(e) {
+            const { key } = e
+            this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
+        },
+        handleLogin() {
+            this.$refs.loginForm.validate(valid => {
+                if (valid) {
+                    this.loading = true
+                    this.showLog = false
+
+                    firebase.auth().signInWithEmailAndPassword(this.loginForm.username, this.loginForm.password).then(cred =>
+                    {
+                        this.loading = false
+                        this.$router.push({ name: 'HomeUser'}) //after login, direct to home user page
+                    }).catch(err => {
+                        this.loading = false
+                        this.showLog = true
+                        this.messages = err
+                    })
+                }
+            })
+        },
+    },
+    beforeCreate() {
+        document.body.className = "body-bg-image";
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+    min-height: 100%;
+    width: 100%;
+    overflow: hidden;
+    position: absolute;
+
+    .login-form {
+        position: relative;
+        width: 520px;
+        max-width: 100%;
+        padding: 0 35px 0;
+        margin: 0 auto;
+        overflow: hidden;
+        background: #ffffff;
+        margin-top: 50px;
+
+        .btn-submit {
+            width: 100%;
+            border: none;
+            border-radius: 7px;
+            padding: 12px;
+            margin-bottom: 50px;
+            background: #ec407a;
+            color: #ffffff;
+            margin-top: 20px;
+        }
+    }
+}
+</style>
+
+<style lang="scss">
+/* reset element-ui css */
+.login-container {
+    .el-image {
+        width: 100%;
+        text-align: center;
+        margin-top: 50px;
+
+        img {
+            width: 100%;
+            height: 230px;
+        }
+    }
+
+    .el-input {
+        display: inline-block;
+        height: 47px;
+        width: 100%;
+        float: left;
+
+        input {
+            background: transparent;
+            border: 0px;
+            -webkit-appearance: none;
+            border-radius: 0px;
+            padding: 0;
+            margin-bottom: 0;
+            color: #000;
+            height: 47px;
+        }
+    }
+
+    .el-form-item {
+        margin-top: 20px;
+
+        .el-input {
+            border-bottom: 1px solid #BDBDBD;
+            color: #454545;
+        }
+    }
+
+    .el-form-item__content {
+        .el-form-item__error {
+            color: #ff0000;
+        }
+    }
+}
+</style>
---
+++ src/components/profile/UserProfile.vue
@@ -0,0 +1,223 @@
+<template>
+    <div class="profile container"
+        v-if="profile">
+
+        <div class="wallpaper">
+            <img class="wp" src="@/assets/bg.jpg">
+        </div>
+
+        <div class="profile_img">
+            <img class="profile-ava" 
+                :src="profile.avatar">
+        </div>
+
+        <div class="profile_information"
+            v-if="profile">
+            <h2 class="profile-name">
+                {{profile.fullname}}
+            </h2>
+            <ul>
+                <li><a href="">Follower </a><span>90000</span></li>
+                <li><a href="">Following </a><span>100</span></li>
+                <li><router-link 
+                    :to="{ name: 'EditUserProfile', params: {id: this.profile.id}}">
+                    Edit Information
+                </router-link>
+                </li>
+            </ul>
+            <div>
+                <router-link 
+                    :to="{name: 'NewPost' }" 
+                    class="btn-small pink lighten-1 btn-new-post ">
+                    New Post
+                </router-link>
+            </div>
+            <div class="album" 
+                v-if="posts">
+                <div class="card" 
+                    v-for="post in posts" 
+                    :key="post.id">
+                    <div class="card-image">
+                        <img class="activator" :src="post.image">
+                    </div>
+
+                    <div class="card-content post-info">
+                        <span class="card-title activator grey-text text-darken-4 post-title">
+                            {{ post.title }}
+                        </span>
+                        <p class="grey-text text-darken-4">
+                            {{post.timestamp}} 
+                            <span class="like-count">
+                                &#128151;
+                            </span>
+                            {{ post.like }}
+                        </p>
+                        <a class="btn grey lighten-2 btn-like" 
+                            :class="{'pink-text' : post.has_been_liked}" 
+                            @click="likePost(post.id)">
+                            <i class="material-icons">
+                                favorite
+                            </i>
+                        </a>
+                   
+                    </div>  
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+import db from '@/firebase/init'
+import moment from 'moment'
+
+export default {
+    name: 'UserProfile',
+    data(){
+        return{
+            profile: null,
+            user_id: null,
+            posts:  []
+        }
+    },
+    beforeCreate(){
+        document.body.className = "body-bg-no-image";
+        //get the user slug from users collection
+        let ref = db.collection('users')
+        ref.doc(this.$route.params.id).get().then(user => 
+        {
+            this.profile =user.data()
+            this.profile.id = user.id   //set id of this profile to id of the user in users collection
+        })
+        firebase.auth().onAuthStateChanged((user) =>
+        { //check the current status of the user
+            if(user){
+                this.user_id = user.uid; //get the account uid
+                let postdb = db.collection('posts').where('user_id', '==', this.user_id)//using account uid to find user own post and get post data
+                postdb.get().then(snapshot => 
+                {
+                    if(snapshot){
+                    snapshot.forEach(doc =>
+                        {
+                            let post = doc.data()
+                            post.id = doc.id
+                            post.timestamp = moment(doc.data().timestamp).format('lll')
+                            this.posts.push(post)
+                        })
+                    }
+                })
+                
+            }
+            else
+            {
+                this.user = null
+            }
+        })
+    },
+    created(){
+        
+    }
+}
+</script>
+
+<style>
+    .profile{
+        box-shadow:0px 0px 20px grey;
+        position: relative;
+        width: 50%;
+        
+    }
+    .wp{
+        display: block;
+        width: 100%;
+        height: 300px;
+    }
+    .profile-ava{
+        border: 5px solid rgb(218, 218, 218);
+        border-radius: 50%;
+        display: block;
+        width: 13em;
+        height: 13em;
+        position: absolute;
+        margin: -115px 0 auto 1em;  
+    }
+    .profile_information{
+        background-color: white;
+        height: auto;
+        margin-bottom: 40px;
+    }
+    .profile-name{
+        color: rgb(39, 21, 21);
+        font-size: 2.5em;
+        font-weight: 600;
+        margin:0 0 0 6.5em;
+        text-shadow:-2px   -2px #F3F3F3,
+                                 -2px -1.5px #F3F3F3,
+                                 -2px   -1px #F3F3F3,
+                                 -2px -0.5px #F3F3F3,
+                                 -2px    0px #F3F3F3,
+                                 -2px  0.5px #F3F3F3,
+                                 -2px    1px #F3F3F3,
+                                 -2px  1.5px #F3F3F3,
+                                 -2px    2px #F3F3F3,
+                               -1.5px    2px #F3F3F3,
+                                 -1px    2px #F3F3F3,
+                               -0.5px    2px #F3F3F3,
+                                  0px    2px #F3F3F3,
+                                0.5px    2px #F3F3F3,
+                                  1px    2px #F3F3F3,
+                                1.5px    2px #F3F3F3,
+                                  2px    2px #F3F3F3,
+                                  2px  1.5px #F3F3F3,
+                                  2px    1px #F3F3F3,
+                                  2px  0.5px #F3F3F3,
+                                  2px    0px #F3F3F3,
+                                  2px -0.5px #F3F3F3,
+                                  2px   -1px #F3F3F3,
+                                  2px -1.5px #F3F3F3,
+                                  2px   -2px #F3F3F3,
+                                1.5px   -2px #F3F3F3,
+                                  1px   -2px #F3F3F3,
+                                0.5px   -2px #F3F3F3,
+                                  0px   -2px #F3F3F3,
+                               -0.5px   -2px #F3F3F3,
+                                 -1px   -2px #F3F3F3,
+                               -1.5px   -2px #F3F3F3;
+    }
+    .profile ul{
+        margin-top: 2px;
+        margin-left: 17em;
+
+    }
+    ul li {
+        display: inline-block;
+        margin-right: 5em;
+        margin-top:0.5em;
+    }
+    .btn-new-post{
+        display: block;
+        margin: auto;
+        width: 95%;
+    }
+    .album{
+        border-top: 3px solid black;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 30px;
+        margin: 10px 20px auto;
+    }
+    .card{
+        display: inline-block;
+    }
+    .card .card-content{
+        padding: 0;
+    }
+    .ab{
+        width: 100%;
+        height: 100%;
+        padding: none;
+    }
+</style>
